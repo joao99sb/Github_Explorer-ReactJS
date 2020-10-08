5c07d7b17d0a6afde1f994d45537104431a56d1e
João Victor Dias <joaovictor665@gmail.com>
2020-10-08 20:05:04 -0300

    rotes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
+import Repository from '../pages/Repository';
 
 const Routes: React.FC = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Dashboard} />
-      <Route path="/repository" />
+      <Route path="/repository" component={Repository} />
     </Switch>
   </BrowserRouter>
 );

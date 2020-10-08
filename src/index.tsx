3aa175adc5fc798a9203063ff10e999826fb3ab6
João Victor Dias <joaovictor665@gmail.com>
2020-10-08 20:00:06 -0300

    rotes

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import GlobalStyle from './styles/global';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    <GlobalStyle />
+  </>,
   document.getElementById('root'),
 );
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore Repositórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer " />
+      <Title>Explore Repositórios no GitHub</Title>
+    </>
+  );
 };
 
 export default Dashboard;

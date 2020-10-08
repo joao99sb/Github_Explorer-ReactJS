bab8cabce46135452a05198da33194db0672058d
João Victor Dias <joaovictor665@gmail.com>
2020-10-08 18:50:14 -0300

    start again

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './App.css';
 
 const App: React.FC = () => {
-  return <h1>OI</h1>;
+  return (
+    <>
+      <h1>OI</h1>
+    </>
+  );
 };
 
 export default App;
